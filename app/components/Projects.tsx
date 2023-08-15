6c1e12fb4fc89acc3f26bb24f4801027798ca73f
unknown <rmmccar92@gmail.com>
2023-08-15 15:26:31 -0400

    test 2?

---
+++ app/components/Projects.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+
+interface ProjectsProps {}
+
+const Projects: FC<ProjectsProps> = ({}) => {
+  return (
+    <div>
+      <h1>Projects</h1>
+    </div>
+  );
+};
+export default Projects;
