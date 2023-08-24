ddd88364df8d0eed674d88a2226a73503747c6dc
unknown <rmmccar92@gmail.com>
2023-08-24 13:52:25 -0400

    added image to hero and styled

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,23 +5,25 @@ interface HeroProps {}
 
 const Hero: FC<HeroProps> = ({}) => {
   return (
-    <section className={styles.hero}>
-      <div className={styles.heroContainer}>
-        <div className={styles.heroContent}>
-          <div className={styles.heroShapes}>
-            <div className={styles.heroElement} />
-            <div className={styles.heroLine} />
-          </div>
-          <div>
-            <h1>
-              Hi, I'm
-              <span className={styles.heroTitleSpan}> Ryan</span>
-            </h1>
-            <p>I'm a full-stack developer and designer in training.</p>
+    <>
+      <section className={styles.hero}>
+        <div className={styles.heroContainer}>
+          <div className={styles.heroContent}>
+            <div className={styles.heroShapes}>
+              <div className={styles.heroElement} />
+              <div className={styles.heroLine} />
+            </div>
+            <div>
+              <h1>
+                Hi, I'm
+                <span className={styles.heroTitleSpan}> Ryan</span>
+              </h1>
+              <p>I'm a full-stack developer.</p>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,30 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "./styles/page.module.css";
 import Hero from "./components/Hero";
 import About from "./components/About";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <>
+    <div className={styles.pageContent}>
       <main className={styles.pageMain}>
         <div className={styles.center}>
           <Hero />
         </div>
       </main>
+      <Image
+        className={styles.treeImage}
+        src="/tree.png"
+        alt="tech-tree-image"
+        fill
+        style={{
+          opacity: "100%",
+          zIndex: 0,
+          objectFit: "contain",
+          objectPosition: "right",
+          aspectRatio: "1/1",
+        }}
+      />
       <About />
-    </>
+    </div>
   );
 }
