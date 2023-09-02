054f601445916413b9b7f75a2ba5e8efbbcef299
unknown <rmmccar92@gmail.com>
2023-09-02 19:39:59 -0400

    added more projects

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,48 +1,46 @@
 const data = {
   projects: [
     {
-      title: "Confused Gamer",
-      link: "https://rmmccar92.github.io/Confused-Gamer/",
-      description:
-        "An application designed to recommend and provide information about video games.",
-      repo: "https://github.com/rmmccar92/Confused-Gamer",
-      image: "/images/ProjectImgs/CGImage.png",
-      technologies: ["HTML5", "CSS3", "JavaScript", "jQuery", "Materialize"],
+      title: "Student Tracker",
+      link: "https://student-sorter-6f8yccr17-rmmccar92.vercel.app/",
+      description: "An application used to sort students into groups.",
+      repo: "https://github.com/rmmccar92/Student-Sorter",
+      image: "/images/ProjectImgs/unc.png",
+      technologies: [
+        "JavaScript",
+        "TypeScript",
+        "NextJS",
+        "NextAuth",
+        "React-Spring",
+      ],
       contributors: [
         { name: "Ryan McCarthy", ghLink: "https://github.com/rmmccar92" },
-        { name: "Aubrey Henley", ghLink: "https://github.com/aubrihenley" },
-        { name: "Daniel Bradley", ghLink: "https://github.com/WebDeverDan" },
-        { name: "Joey Swafford", ghLink: "https://github.com/joeyswafford" },
       ],
     },
     {
-      title: "Farmer's Market Search",
-      link: "https://still-tor-25996.herokuapp.com/",
-      description:
-        "A full stack application that allows local farmers and customers to congregate and provide information about nearby farmer's markets.",
-
-      repo: "https://github.com/rmmccar92/Farmers-Market-Search",
-      image: "/images/ProjectImgs/fmsearchimg2.png",
+      title: "Character Sheet",
+      link: "https://character-sheet-rjjz77zy0-rmmccar92.vercel.app/",
+      description: "Character Creator for Pathfinder",
+      repo: "https://github.com/rmmccar92/Character-Sheet",
+      image: "",
       technologies: [
-        "HTML5",
-        "CSS3",
         "JavaScript",
-        "HandleBars",
-        "NodeJS",
-        "Express",
-        "Bootstrap",
-        "MySql",
-        "GSAP",
-        "Heroku",
+        "TypeScript",
+        "Remix",
+        "Mantine",
+        "Prisma",
+        "MongoDB",
+        "AWS",
+        "Bcrypt",
       ],
       contributors: [
         { name: "Ryan McCarthy", ghLink: "https://github.com/rmmccar92" },
-        { name: "Casey Twine", ghLink: "https://github.com/ElusiveSkies" },
+        { name: "Sean McCarthy", ghLink: "https://github.com/McTastic" },
       ],
     },
     {
       title: "PodCentral",
-      link: "https://pod-central.herokuapp.com/",
+      link: "https://pod-central.onrender.com/",
       description:
         "PodCentral is a full stack web application that allows users to browse their favorite podcasts and to create a podcast of their own and upload episodes.",
       repo: "https://github.com/rmmccar92/PodCentral",
@@ -112,6 +110,46 @@ const data = {
       ],
       contributors: [
         { name: "Ryan McCarthy", ghLink: "https://github.com/rmmccar92" },
+      ],
+    },
+    {
+      title: "Confused Gamer",
+      link: "https://rmmccar92.github.io/Confused-Gamer/",
+      description:
+        "An application designed to recommend and provide information about video games.",
+      repo: "https://github.com/rmmccar92/Confused-Gamer",
+      image: "/images/ProjectImgs/CGImage.png",
+      technologies: ["HTML5", "CSS3", "JavaScript", "jQuery", "Materialize"],
+      contributors: [
+        { name: "Ryan McCarthy", ghLink: "https://github.com/rmmccar92" },
+        { name: "Aubrey Henley", ghLink: "https://github.com/aubrihenley" },
+        { name: "Daniel Bradley", ghLink: "https://github.com/WebDeverDan" },
+        { name: "Joey Swafford", ghLink: "https://github.com/joeyswafford" },
+      ],
+    },
+    {
+      title: "Farmer's Market Search",
+      link: "https://still-tor-25996.herokuapp.com/",
+      description:
+        "A full stack application that allows local farmers and customers to congregate and provide information about nearby farmer's markets.",
+
+      repo: "https://github.com/rmmccar92/Farmers-Market-Search",
+      image: "/images/ProjectImgs/fmsearchimg2.png",
+      technologies: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "HandleBars",
+        "NodeJS",
+        "Express",
+        "Bootstrap",
+        "MySql",
+        "GSAP",
+        "Heroku",
+      ],
+      contributors: [
+        { name: "Ryan McCarthy", ghLink: "https://github.com/rmmccar92" },
+        { name: "Casey Twine", ghLink: "https://github.com/ElusiveSkies" },
       ],
     },
   ],
