c87c952d8172f75ff0566229a18d066210676213
unknown <rmmccar92@gmail.com>
2023-09-09 19:53:16 -0400

    added contact form

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -7,6 +7,25 @@ const Contact: FC<ContactProps> = ({}) => {
   return (
     <div className={styles.contactMain}>
       <h1>Contact</h1>
+      <div className={styles.contactContainer}>
+        <form>
+          <div className={styles.formGroup}>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="message">Message</label>
+            <textarea id="message" name="message" />
+          </div>
+          <div className={styles.formGroup}>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
