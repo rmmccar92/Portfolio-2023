9f7669711f03e3533593fa2131cac79329f81b94
unknown <rmmccar92@gmail.com>
2023-09-11 11:44:47 -0400

    icon test 4

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -34,7 +34,7 @@ const Work: FC<WorkProps> = ({}) => {
                         width={35}
                         height={35}
                         alt="github"
-                        src={require("/public/images/technologies/Github.png")}
+                        src="/images/technologies/Github.png"
                       />
                     </button>
                   </a>
